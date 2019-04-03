d8c5a225b330e41cada69137d1bbccbe9d312b0e
ct2995677 <48548862+ct2995677@users.noreply.github.com>
2019-04-03 19:09:32 +0800

    Add files via upload

---
+++ zhuanhuan.java
@@ -0,0 +1,14 @@
+public class zhuanhuan
+{
+	public static void main(String[] args)
+	{
+		int shu = 48;
+		char y = f(shu);
+		System.out.print(y);
+	}
+	public static char f(int shu)
+	{
+		 char y = (char)shu;
+		return y;
+	}
+}
